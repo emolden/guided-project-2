08cff3db9eed983e3deee0312fd0ebe0faff3821
emolden <emolden@travelers.com>
2025-03-11 14:43:59 -0400

    characters api get route working

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'swapi';
+const CharactersCollection = 'characters';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +16,19 @@ const planets = [
 app.get('/api/planets', (req, res) => {
 res.json(planets);
 })
+
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(CharactersCollection);
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error('Error: ', err);
+        res.status(500).send("Error getting characters");
+    }
+});
 
 
 
