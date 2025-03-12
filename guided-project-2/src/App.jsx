463f336f299fc878ebb3ac16a9f85a065cb33c76
emolden <emolden@travelers.com>
2025-03-12 11:19:37 -0400

    added more fetch functions and mapping character list on the dom

--- guided-project-2/src/App.jsx
+++ guided-project-2/src/App.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [characters, setCharacters] = useState(0)
+  const [characters, setCharacters] = useState(null)
+  const [films, setFilms] = useState(null)
+  const [planets, setPlanets] = useState(null)
+  const [characterById, setCharacterById] = useState(null)
 
   useEffect (() => {
-    const fetchCharacter = async ()=> { 
+    const fetchCharacters = async ()=> { 
       try {
         const response = await fetch("http://localhost:3000/api/characters");
         if (!response.ok) {
@@ -19,18 +22,68 @@ function App() {
       }
 
     };
-    fetchCharacter();
+
+    const fetchFilms = async ()=> { 
+      try {
+        const response = await fetch("http://localhost:3000/api/films");
+        if (!response.ok) {
+          throw new Error('Data could not be fetched!');
+        }
+        const json_response = await response.json();
+        setFilms(json_response);
+        console.log(json_response);
+      } catch (err) {
+        console.error("Error fetching films", err);
+      }
+
+    };
+
+    const fetchPlanets = async ()=> { 
+      try {
+        const response = await fetch("http://localhost:3000/api/planets");
+        if (!response.ok) {
+          throw new Error('Data could not be fetched!');
+        }
+        const json_response = await response.json();
+        setPlanets(json_response);
+        console.log(json_response);
+      } catch (err) {
+        console.error("Error fetching planets", err);
+      }
+
+    };
+
+    const fetchCharacterById = async (id)=> { 
+      try {
+        const response = await fetch(`http://localhost:3000/api/characters/${id}`);
+        if (!response.ok) {
+          throw new Error('Data could not be fetched!');
+        }
+        const json_response = await response.json();
+        setCharacterById(json_response);
+        console.log(json_response);
+      } catch (err) {
+        console.error("Error fetching character by id", err);
+      }
+
+    };
+
+
+    fetchCharacters();
+    fetchFilms();
+    fetchPlanets();
+    fetchCharacterById(1);
 
   }, []
   );
   return (
     <>
       <h1>Characters</h1>
-      {/* <u1>
-        {characters.map((character) =>
-
-        )}
-      </u1> */}
+      <ul>
+        {
+          characters.map((char) => ( <li key={char._id}>{char.name}</li>))
+        }
+        </ul>
     </>
   )
 }
